adb2d67fc006f9cba8e759d7e7c7b0abb5ca4c5b
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-07-20 14:58:21 +0600

    fetch data -axios , use to react app

---
+++ Redux/p9/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to react redux</h1>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
+
+//1. constants define
+//2. asyncronous action creator
+//3. reducer
+//4. store create
+//5. provite react app
+//6. use to react app
+
+//need to install : npm install redux react-redux axios redux-thunk
