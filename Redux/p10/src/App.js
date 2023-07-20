07b9e509db14fb747dda381e522c2e68b91f8d46
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-07-20 16:42:06 +0600

    using redux toolkit - counter

---
+++ Redux/p10/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CounterView from "./features/counter/CounterView";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <CounterView />
+    </div>
+  );
+}
+
+export default App;
