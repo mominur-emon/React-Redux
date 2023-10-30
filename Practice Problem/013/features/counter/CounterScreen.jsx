4639675ae8d4a08803ef5d681babd8c5268af3bf
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-10-30 11:28:11 +0600

    redux toolkit -counter

---
+++ Practice Problem/013/features/counter/CounterScreen.jsx
@@ -0,0 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByNumber, reset } from "./counterSlice";
+
+const CounterScreen = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        {" "}
+        Decrement{" "}
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByNumber(5));
+        }}
+      >
+        Increment_By_Number
+      </button>
+    </div>
+  );
+};
+
+export default CounterScreen;
